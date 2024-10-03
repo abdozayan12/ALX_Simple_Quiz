5bc941c904762c0dd0a55dc9e325e4f5561cdc8d
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-10-03 22:14:47 +0300

    Update quiz.js

---
+++ quiz.js
@@ -0,0 +1,7 @@
+const rightanswer = document.getElementById("choice1")
+const wronganswer = document.getElementById("choice2")
+const wronganswer2 = document.getElementById("choice3")
+
+function checkAnswer() {
+    
+}
