2bcdb70d1069535e5d98428a7ad7922c2eb8a5aa
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-10-04 14:37:19 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,22 @@
 const rightanswer = document.getElementById("choice1")
 const wronganswer = document.getElementById("choice2")
 const wronganswer2 = document.getElementById("choice3")
+const submit = document.getElementById("submit-answer")
+
+submit.addEventListener("click", checkAnswer)
+
 
 function checkAnswer() {
+if (rightanswer.checked == true) {
+    alert("Correct!")
+  if (wronganswer.checked == true) {
+    alert("Incorrect")
+  }
+    if (wronganswer2.checked == true) {
+        alert("Incorrect")
+    }
+    else {
+        alert("Please select an answer")
+    }  
     
-}
+}}
